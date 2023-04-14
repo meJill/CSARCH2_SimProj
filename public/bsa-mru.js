d566d89354f5814d069b1936f7241f15eab5846b
John Wesley Abe <68657618+Frieya@users.noreply.github.com>
2023-04-15 03:52:15 +0800

    functions for the bsa-mru functionality
    still not tested.

--- public/bsa-mru.js
+++ public/bsa-mru.js
@@ -1,18 +1,93 @@
 $(document).ready(function () {
-    var Set = /** @class */ (function () {
-        function Set(s_size, b_size) {
-            this.set_blocks = [];
-            this.set_size = s_size;
-            for (var i = 0; i < b_size; i++) {
+    class Block {
+        constructor(tag, data) {
+          this.tag = tag;
+          this.data = data;
+        }
+      }
+      
+      class CacheSet {
+        constructor(numBlocks) {
+          this.numBlocks = numBlocks;
+          this.blocks = new Array(numBlocks).fill(null);
+          this.mruIndex = 0;
+        }
+      
+        getBlock(tag) {
+          for (let i = 0; i < this.blocks.length; i++) {
+            const block = this.blocks[i];
+            if (block !== null && block.tag === tag) {
+              this.mruIndex = i;
+              return block.data;
             }
+          }
+          return null;
         }
-        return Set;
-    }());
-    var Block = /** @class */ (function () {
-        function Block() {
+      
+        addBlock(tag, data) {
+          for (let i = 0; i < this.blocks.length; i++) {
+            if (this.blocks[i] === null) {
+              this.blocks[i] = new Block(tag, data);
+              this.mruIndex = i;
+              return;
+            }
+          }
+          this.blocks[this.mruIndex] = new Block(tag, data);
         }
-        return Block;
-    }());
+      }
+      
+      class BlockSetAssociativeCache {
+        constructor(configuration, cache_size, set_size, block_size) {
+            this.set_size = set_size
+            this.block_size = block_size
+            if (configuration="Word"){
+                this.numBlocksinCache = cache_size/block_size
+                this.numSets = this.numBlocksinCache/set_size
+            }  
+            if (configuration="Block"){
+                this.numBlocksinCache = cache_size
+                this.numSets = cache_size/set_size
+            } 
+            this.sets = new Array(numSets)
+                .fill(null)
+                .map(() => new CacheSet(set_size));
+            this.hits = 0;
+            this.misses = 0;
+        }
+      
+        read_word(address) {
+          const tag = Math.floor(
+            address / (this.numSets * this.block_size)
+          );
+          const set_num = Math.floor(address / this.block_size) % this.numSets;
+          const blockData = this.sets[set_num].getBlock(tag);
+          if (blockData !== null) {
+            this.hits++;
+            return blockData;
+          }
+          this.misses++;
+          const newData = `Data from memory for address ${address}`;
+          this.sets[setIndex].addBlock(tag, newData);
+          return newData;
+        }
+
+        read_block(block) {
+            const tag = Math.floor(
+              address / (this.numSets)
+            );
+            const setIndex = Math.floor(address) % this.numSets;
+            const blockData = this.sets[setIndex].getBlock(tag);
+            if (blockData !== null) {
+              this.hits++;
+              return blockData;
+            }
+            this.misses++;
+            const newData = `Data from memory for block ${block}`;
+            this.sets[setIndex].addBlock(tag, newData);
+            return newData;
+          }
+      }
+      
     $(".dropdown-toggle").next(".dropdown-menu").children().on("click", function () {
         $(this).closest(".dropdown-menu").prev(".dropdown-toggle").text($(this).text());
     });
